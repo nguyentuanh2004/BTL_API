4ff6a74113c31985341b7acb62227243965826ab
Nguyễn Tú Anh <anh.nt.004@gmail.com>
2025-03-27 17:50:51 +0700

    clerk authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import ThemeCom from '@/components/ThemeCom';
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +21,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeCom>
+            <Header/>
+            {children}
+          </ThemeCom>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+      <ul className='flex gap-4'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href={'/sign-in'}>Sign in</Link>
+        </SignedOut>
+        <li className='hidden sm:block'>
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li className='hidden sm:block'>
+          <Link href={'/favorites'}>Favorites</Link>
+        </li>
+        <li className='hidden sm:block'>
+          <Link href={'/about'}>About</Link>
+        </li>
+      </ul>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className='text-2xl font-bold bg-pink-300 py-1 px-2 rounded-lg'>
+            Tulip
+          </span>
+          <span className='text-xl hidden sm:inline'>TA</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
