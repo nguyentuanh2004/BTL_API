dedb042fe3f6bbf26fda39183a773d77c1632735
Nguyễn Tú Anh <anh.nt.004@gmail.com>
2025-04-01 00:28:20 +0700

    inn

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -5,6 +5,6 @@ import { helloWorld } from "../../../inngest/functions";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld, // <-- This is where you'll always add all your functions
+    helloWorld, // <-- This is where you'll always add all your functions...
   ],
 });
